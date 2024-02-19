cd9b3c42438cb785072d094e4eb7d475006e5561
MosheMatof <73488759+MosheMatof@users.noreply.github.com>
2024-02-20 00:39:27 +0200

    recursive folders

--- utilles.py
+++ utilles.py
@@ -4,10 +4,11 @@ import pandas as pd
 files_path = './sfarim'
 def generate_csv_from_txt(csv_file_path, folder_path = files_path):
     data = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".txt"):
-            with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as file:
-                content = file.read()
-            data.append((filename, content))
+    for dirpath, dirnames, filenames in os.walk(folder_path):
+        for filename in filenames:
+            if filename.endswith(".txt"):
+                with open(os.path.join(dirpath, filename), 'r', encoding='utf-8') as file:
+                    content = file.read()
+                data.append((filename, content))
     df = pd.DataFrame(data, columns=['name', 'content'])
     df.to_csv(csv_file_path, index=False)
