641230fe27d46183a0b8cd66b3cd34f7b826e1c5
MosheMatof <73488759+MosheMatof@users.noreply.github.com>
2024-02-19 23:12:18 +0200

    fix

--- utilles.py
+++ utilles.py
@@ -6,7 +6,7 @@ def generate_csv_from_txt(csv_file_path, folder_path = files_path):
     data = []
     for filename in os.listdir(folder_path):
         if filename.endswith(".txt"):
-            with open(os.path.join(folder_path, filename), 'r') as file:
+            with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as file:
                 content = file.read()
             data.append((filename, content))
     df = pd.DataFrame(data, columns=['name', 'content'])
