f13f61854f0500ba31c2f876ddb2ec705661fc42
חיים צבי פומרנציק <pomeranc@g.jct.ac.il>
2024-02-20 18:55:44 +0200

    brahot

--- utilles.py
+++ utilles.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-files_path = './sfarim/baycha'
+files_path = './sfarim/brachot'
 def generate_csv_from_txt(csv_file_path, folder_path = files_path):
     data = []
     for dirpath, dirnames, filenames in os.walk(folder_path):
