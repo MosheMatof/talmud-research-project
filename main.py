a4d123361e83157330b4e145614dd5e47daa7abc
חיים צבי פומרנציק <pomeranc@g.jct.ac.il>
2024-02-25 21:58:41 +0200

    brahot

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from utilles import generate_csv_from_txt
 from visualizer import plot_vectors
 import pandas as pd
 
-# generate_csv_from_txt('./sfarim.csv')
-# df = pd.read_csv('./sfarim.csv')
-# generate_vectors(df,'name', 'content', 'vectors.pkl')
+generate_csv_from_txt('./sfarim.csv')
+df = pd.read_csv('./sfarim.csv')
+generate_vectors(df,'name', 'content', 'vectors.pkl')
 train_multiple_classifiers('vectors.pkl')
-# plot_vectors('vectors.pkl')
+plot_vectors('vectors.pkl')
